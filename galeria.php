9405d2798c8bf8b26e6af3c47947cc10c1936fbf
Israel Sales <israelsales@outlook.com>
2024-10-24 14:43:53 -0300

    Fix: Ajuste na pagina de galeria

--- galeria.php
+++ galeria.php
@@ -2,8 +2,8 @@
 <html lang="pt-br">
 
 <head>
-<!-- Head -->
- <?php include('./snippets/head.html'); ?>
+    <!-- Head -->
+    <?php include('./snippets/head.html'); ?>
 </head>
 
 <body data-spy="scroll" data-target=".navbar" data-offset="70">
@@ -11,15 +11,17 @@
     <!-- Barra de Navegação -->
     <?php include('./snippets/navegacao-galeria.html'); ?>
 
-    <!-- Modal de Versões Anteriores -->
-    <?php include('./snippets/botao-flutuante.html'); ?>
+    <!-- Sessão Home -->
+    <?php include('./snippets/home.html'); ?>
 
-	<!--Galeria -->
-	<main class="content-scroll" id="galeria">
-		<?php include ('./snippets/galeria.html');?>
-	</main>
+    <!--Galeria -->
+    <?php include('./snippets/galeria.html'); ?>
+
     <!-- Sessão dos Patrocinadores -->
     <?php include('./snippets/patrocinio.html'); ?>
+
+    <!-- Modal de Versões Anteriores -->
+    <?php include('./snippets/botao-flutuante.html'); ?>
 
     <!-- Footer -->
     <?php include('./snippets/rodape.html'); ?>
